dc7681d8e8c333b649ddd0886fb4515f386c518d
Aman Anwar <aman.anwar18oct@gmail.com>
2026-01-19 16:38:30 -0500

    added socials

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -14,22 +14,22 @@ function Header() {
             <h2 className="text-xl pt-2 atkinson-hyperlegible-next-regular" style={{ color: '#fcecc9' }}>
 I’m a 2nd-year CS major at Virginia Tech. I love all things audio and currently lead a 5-person team on spatial audio research at VT. I’m an incoming SWE intern at Knot and a research intern at the IDOPT Lab on the NASA RAM project.</h2>
             <div className="flex gap-4 pt-6">
-                <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.linkedin.com/in/aman-anwar-/" target="_blank" rel="noopener noreferrer">
                     <FaLinkedinIn className="social-icon" />
                 </a>
-                <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/notcolumbus" target="_blank" rel="noopener noreferrer">
                     <PiGithubLogoFill className="social-icon" />
                 </a>
-                <a href="https://devpost.com/yourusername" target="_blank" rel="noopener noreferrer">
+                <a href="https://devpost.com/aman-anwar18oct" target="_blank" rel="noopener noreferrer">
                     <SiDevpost className="social-icon" />
                 </a>
-                <a href="https://youtube.com/yourchannel" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.youtube.com/@amananwar7421" target="_blank" rel="noopener noreferrer">
                     <FaYoutube className="social-icon" />
                 </a>
-                <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer">
+                <a href="https://x.com/Not_Columbus" target="_blank" rel="noopener noreferrer">
                     <RiTwitterXLine className="social-icon" />
                 </a>
-                <a href="https://instagram.com/yourusername" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.instagram.com/notorious.columbus/" target="_blank" rel="noopener noreferrer">
                     <RiInstagramFill className="social-icon" />
                 </a>
             </div>
